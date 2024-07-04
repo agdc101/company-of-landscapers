158e2a31415cdeb87912e8279fd154f9c859c44f
agdc101 <agdc101@gmail.com>
2024-07-04 10:39:36 +0100

    feat: fm installed. mobile nav animated

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Hamburger from 'hamburger-react';
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Nav = () => {
     const [isOpen, setOpen] = useState(false);
@@ -15,7 +16,7 @@ const Nav = () => {
 
     return (
         <header className="flex justify-between content-center px-2 lg:px-4 z-30 relative text-white h-0">
-            <h1 className="card-header-title text-lg lg:text-2xl lg:font-light pt-3">The Company Of Landscapers</h1>
+            <motion.h1 className="card-header-title z-40 text-xl lg:text-2xl lg:font-light pt-3.5" animate={{color: isOpen ? '#000000' : '#FFFFFF' }} transition={{ delay: 0.4, ease: "easeIn" }}>The Company Of Landscapers</motion.h1>
             <nav className="hidden md:flex justify-between pt-3">
                 <ul className="card-header-icon md:flex text-lg font-light">
                     <li className="px-4"><Link to="/" className="font-light">Home</Link></li>
@@ -28,18 +29,20 @@ const Nav = () => {
             <div className="relative z-50 flex justify-end m-2 ml-auto">
                 <Hamburger className="bg-red" toggled={isOpen} toggle={setOpen} color={isOpen ? 'black' : 'white'}/>
             </div>
-            {isOpen &&
-                <nav className="md:hidden">
-                    <div className="bg-white absolute top-0 left-0 right-0 bottom-0 z-30 h-screen" >
-                        <ul className="card-header-icon text-lg font-light mt-16">
-                            <li className="p-6 text-2xl"><Link to="/" className="font-light text-black">Home</Link></li>
-                            <li className="p-6 text-2xl"><Link to="/about" className="font-light text-black">About</Link></li>
-                            <li className="p-6 text-2xl"><Link to="/our-work" className="font-light text-black">Our Work</Link></li>
-                            <li className="p-6 text-2xl"><Link to="/contact" className="font-light text-black">Contact</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-            }
+            <AnimatePresence>
+                {isOpen &&
+                    <nav>
+                        <motion.div className="bg-white absolute top-0 left-0 right-0 bottom-0 z-30 h-screen" initial={{ x: -800 }} animate={{ x: 0 }} exit={{ x: -800 }} transition={{ type: "tween", duration: 0.6 }} >
+                            <ul className="card-header-icon font-light mt-24">
+                                <li className="p-7 text-3xl"><Link to="/" className="font-light text-black">Home</Link></li>
+                                <li className="p-7 text-3xl"><Link to="/about" className="font-light text-black">About</Link></li>
+                                <li className="p-7 text-3xl"><Link to="/our-work" className="font-light text-black">Our Work</Link></li>
+                                <li className="p-7 text-3xl"><Link to="/contact" className="font-light text-black">Contact</Link></li>
+                            </ul>
+                        </motion.div>
+                    </nav>
+                }
+            </AnimatePresence>
             {/* -=-=-=-= */}
         </header>
     )
