2fa28dd5804c0da77b7a5967f81556f02e3d0d3e
agdc101 <agdc101@gmail.com>
2024-07-12 09:03:01 +0100

    styles: header fixed on scroll with anim

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -2,11 +2,17 @@
 import { useState, useEffect } from "react";
 import Hamburger from 'hamburger-react';
 import { NavLink } from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import framerAnimations from "../utils/framer-anims";
 
 const Nav = () => {
     const [isOpen, setOpen] = useState(false);
+
+    const { scrollY } = useScroll();
+
+    const backgroundColor = useTransform(scrollY, [0, 200], ["rgba(0, 0, 0, 0)", "rgba(84, 111, 104, 1)"]);
+    const titleScale = useTransform(scrollY, [0, 200], [1, 1.05]);
+
 
     //disable scrolling when mobile nav is open
     useEffect(() => {
@@ -14,8 +20,8 @@ const Nav = () => {
     }, [isOpen]);
 
     return (
-        <header className="flex justify-between content-center px-2 lg:px-4 z-30 relative text-white h-0">
-            <motion.h1 className="card-header-title z-40 text-lg lg:text-2xl lg:font-light pt-3.5" initial={{ opacity: 0 }} animate={{color: isOpen ? '#000000' : '#FFFFFF', opacity: 1 }} transition={{ delay: 0.75, duration: 1 }}>The Company Of Landscapers</motion.h1>
+        <motion.header className="flex justify-between content-center pl-4 pr-2 lg:px-8 z-30 h-16 fixed text-white w-full" style={{ backgroundColor: backgroundColor }}>
+            <motion.h1 className="card-header-title z-40 text-lg sm:text-xl lg:text-2xl lg:font-light pt-3.5" style={{ scale: titleScale }} initial={{ opacity: 0 }} animate={{color: isOpen ? '#000000' : '#FFFFFF', opacity: 1 }} transition={{ delay: 0.75, duration: 1 }}>The Company Of Landscapers</motion.h1>
             <motion.nav className="hidden md:flex justify-between pt-3 lg:pt-4" {...framerAnimations.fadeIn}>
                 <ul className="card-header-icon md:flex text-lg font-light">
                     <li className="px-4 2xl:px-6 2xl:text-xl"><NavLink to="/" className="font-light">Home</NavLink></li>
@@ -41,7 +47,7 @@ const Nav = () => {
                 </nav>
             }
             {/* -=-=-=-= */}
-        </header>
+        </motion.header>
     )
 }
 
