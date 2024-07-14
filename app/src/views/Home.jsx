550d89e6a69ebae09d63dd8e2567f90581fc37a4
agdc101 <agdc101@gmail.com>
2024-07-14 10:13:51 +0100

    styles: anim added to intro text. mob nav anim needs fix

--- app/src/views/Home.jsx
+++ app/src/views/Home.jsx
@@ -1,22 +1,30 @@
+import { useRef } from "react";
 import { useLoaderData } from 'react-router-dom';
+import { useInView } from "framer-motion";
 import Hero from '../components/Hero';
 
 const Home = () => {
   const loaderData = useLoaderData();
-  console.log(loaderData);
+  console.log('loaderData', loaderData);
+
+  const introTextRef = useRef(null);
+  const introTextIsInView = useInView(introTextRef, { once: true });
 
   const { data, error, loading } = loaderData;
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return <h2 className="text-red-900 text-center mt-6 text-2xl">Error: {error.message}</h2>;
 
   return (
     <>
       <Hero imageUrl={data.entry.heroImage[0].url} imageAlt={data.entry.heroImage[0].alt} title={data.entry.heroTitle} text={data.entry.heroText} />
       <section className="flex justify-around py-20 px-10">
-        <div className="text-center w-3/5 xl:w-2/5">
-        {/* <h1 className="text-6xl">{loaderData.message}</h1> */}
-          <p className="text-4xl">Introduction</p>
+        <div ref={introTextRef} className="text-center w-3/5 xl:w-2/5" style={{
+            transform: introTextIsInView ? "none" : "translateX(-100px)",
+            opacity: introTextIsInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          }}>
+          <p className="text-4xl ">Introduction</p>
           <p className="mt-14 text-xl xl:text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt expedita ex eos similique ducimus aliquam aut animi? Quibusdam libero debitis molestiae reprehenderit reiciendis tenetur. Eum possimus velit ipsum aut voluptates!</p>
         </div>
         <div className="hidden xl:block w-2/5 max-w-screen-md">
