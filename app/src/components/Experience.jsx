e734141c5858bdf1437eeedbed0481b9f0cf4c40
agdc101 <agdc101@gmail.com>
2024-07-18 08:38:05 +0100

    styles: anim added to intro images

--- app/src/components/Experience.jsx
+++ app/src/components/Experience.jsx
@@ -4,6 +4,7 @@ import { useInView } from "framer-motion";
 export default function Experience({homePage}) {
     const expTextRef = useRef(null);
     const expTextIsInView = useInView(expTextRef, { once: true });
+    const expImageRef = useRef(null);
 
     return (
         <section className="flex flex-col items-center xl:flex-row-reverse justify-around py-20 px-2 xl:px-10 bg-slate-200">
@@ -16,7 +17,13 @@ export default function Experience({homePage}) {
                 <p className="my-14 text-xl xl:text-2xl 2xl:text-3xl">{homePage.experienceDescription}</p>
             </div>
             <div className="xl:block w-4/5 sm:w-3/5 md:w-2/5 max-w-screen-md">
-                <img className="rounded shadow-custom" src={homePage.experienceImage[0].url} alt={homePage.experienceImage[0].alt} />
+                <img ref={expImageRef} className="rounded shadow-custom" src={homePage.experienceImage[0].url} alt={homePage.experienceImage[0].alt}
+                    style={{
+                        transform: expTextIsInView ? "none" : "translateX(-100px)",
+                        opacity: expTextIsInView ? 1 : 0,
+                        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                    }}
+                />
             </div>
         </section>
     );
